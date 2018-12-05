82b81514121a52c40e622ad5ff6403e2aa010645
(BG) Team <exceptionhelper@gmail.com>
2018-12-05 08:55:49 +0200

    Update Program.cs

--- CourseProject-1801681029/Task-1-ConvertToInches/Program.cs
+++ CourseProject-1801681029/Task-1-ConvertToInches/Program.cs
@@ -2,12 +2,12 @@
 
 namespace Task_1_Convertion
 {
+    // Условие? И ко й туй '2029'? Яде ли се, пие ли се и защо пък да не е '2030'?
+    // Ако се повтаря няколко пъти '2029', не е ли по-удобно да е изнесено в променлива?
     class Program
     {
         static void Main(string[] args)
         {
-            
-            // Tuka neshto
             Console.WriteLine("2029 to binary is: {0}.", Convert.ToString(2029, 2));
             Console.WriteLine("2029 to octal is: {0}.", Convert.ToString(2029, 8));
             Console.WriteLine("2029 to heximal is: {0}.", Convert.ToString(2029, 16));
